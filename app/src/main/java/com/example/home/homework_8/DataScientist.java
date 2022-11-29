43a9beaebf95b753ccd327cf90ff16969b4204f4
Mikle Zanimonetc <z_a_r_k@mail.ru>
2022-11-29 13:21:01 +0300

    дз_8 пункт 2

--- app/src/main/java/com/example/home/homework_8/DataScientist.java
+++ app/src/main/java/com/example/home/homework_8/DataScientist.java
@@ -1,5 +1,5 @@
 package com.example.home.homework_8;
 
 public class DataScientist {
-    Workplace workplace = new Workplace("HP","Logitech","Logitech","Samsung");
+    Workplace workplace = new Workplace("MacBook");
 }
