000c54b2bbaf76cf9b3de616ea410f5b78ad3e84
Mikle Zanimonetc <z_a_r_k@mail.ru>
2022-11-29 14:17:09 +0300

    дз_8 пункт 4

--- app/src/main/java/com/example/home/homework_8/FakeWorkplace.java
+++ app/src/main/java/com/example/home/homework_8/FakeWorkplace.java
@@ -1,5 +1,5 @@
 package com.example.home.homework_8;
 
 abstract public class FakeWorkplace {
-    Workplace workplace = new Workplace("HP","Logitech","Logitech","Samsung");
+    Workplace workplace = new Workplace("MacBook");
 }
