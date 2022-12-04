d1ce3ee0dc7c619d3d3f422ec0a56535eac58692
Mikle Zanimonetc <z_a_r_k@mail.ru>
2022-12-04 18:30:16 +0300

    дз_9 исправленное v2

--- app/src/main/java/com/example/home/homework_9/Registration.java
+++ app/src/main/java/com/example/home/homework_9/Registration.java
@@ -7,46 +7,46 @@ public class Registration {
         registration();
     }
 
-    static boolean correct(String data) {
-        boolean check;
+    static boolean isInputCorrect(String data) {
         String testWord = "[A-Za-z\\d_]{1,20}";
         if (data.matches(testWord)) {
-            check = true;
+            return true;
         } else {
-            check = false;
+            return false;
         }
-        return check;
     }
 
     static void receptionLoginAndPassword(String login, String password, String correctPassword) throws WrongLoginException, WrongPasswordException {
-        if (correct(login)) {
+        if (isInputCorrect(login)) {
             System.out.println("Логин принят");
         } else {
             throw new WrongLoginException("Некоректно введён логин");
         }
-        if (correct(password)) {
+        if (isInputCorrect(password)) {
+            System.out.println("Пароль принят");
         } else {
             throw new WrongPasswordException("Некорректно введён пароль");
         }
         if (correctPassword.equals(password)) {
-            System.out.println("Пароль принят");
+            System.out.println("Пароли совпадают");
         } else {
-            System.out.println("Пароли не совпадают");
+            throw new WrongPasswordException("Пароли не совпадают");
         }
     }
 
-    static void registration() throws WrongLoginException, WrongPasswordException {
+    static void registration() {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Логин должен содержать только латинские буквы, цифры и знак подчеркивания. Длина логина должна быть меньше 20 символов. \nВведите логин");
+        String login = in.nextLine();
+
+        System.out.println("Пароль должен содержать только латинские буквы, цифры и знак подчеркивания. Длина пароля должна быть меньше 20 символов. \nВведите пароль");
+        String password = in.nextLine();
+
+        System.out.println("Повторно введите пароль");
+
+        String confirmPassword = in.nextLine();
         try {
-            Scanner in = new Scanner(System.in);
-            System.out.println("Логин должен содержать только латинские буквы, цифры и знак подчеркивания. Длина логина должна быть меньше 20 символов. \nВведите логин");
-            String login = in.nextLine();
-
-            System.out.println("Пароль должен содержать только латинские буквы, цифры и знак подчеркивания. Длина пароля должна быть меньше 20 символов. \nВведите пароль");
-            String password = in.nextLine();
-
-            System.out.println("Повторно введите пароль");
-            String confirmPassword = in.nextLine();
-
             receptionLoginAndPassword(login, password, confirmPassword);
         } catch (WrongLoginException errorLogin) {
             System.out.println("Некоректный логин");
